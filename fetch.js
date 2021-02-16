36b7539a6cb0b3def50d31b896b2cd97596c6b90
Lauren <laurengibbonz@gmail.com>
2021-02-15 18:22:27 -0800

    dropdown update

--- fetch.js
+++ fetch.js
@@ -1,30 +1,35 @@
-const lat = '-22.9028',
+const api = '8bd32c8b25477b92c6a32c7c6b7faa13',
+    unit = 'metric';
 
+let lat = '-22.9028',
     long = '-43.2075',
-
-    api = '8bd32c8b25477b92c6a32c7c6b7faa13',
-
-    unit = 'metric',
-
     url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely,alerts&appid=${api}&units=${unit}`;
-
-
 
 console.log(url);
 
 document.querySelector('#cities').addEventListener('change', function(){
-let cityValue = document.querySelector('#cities').value;
+    let cityValue = document.querySelector('#cities').value;
     if(cityValue == 'NYC') {
-        lat = ;
-        long = ;
-        url =  XXX;
+        lat = '40.714272';
+        long = '-74.005966';
+        url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely,alerts&appid=${api}&units=${unit}`;
         createFetch(url)
     } if(cityValue == 'Beijing') {
-        lat = ;
-        long = ;
-        url =  XXX;
+        lat = '39.907501';
+        long = '116.397232';
+        url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely,alerts&appid=${api}&units=${unit}`;
         createFetch(url)
-    } 
+    } if(cityValue == 'Taipei') {
+        lat = '24.94702';
+        long = '121.581749';
+        url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely,alerts&appid=${api}&units=${unit}`;
+        createFetch(url)
+    }  else {
+        lat = '-22.9028';
+        long = '-43.2075';
+        url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely,alerts&appid=${api}&units=${unit}`;
+        createFetch(url)
+    }
 });
 
 
@@ -70,6 +75,4 @@ function createFetch(url) {
     });
 }
 
-
-
-		
+createFetch(url);
